517093536da8981e744961deefcafa1fe5e9ad30
Erin Bergman <erin.e.bergman@gmail.com>
2016-08-17 15:16:57 -0400

    Countries Appgit add .

--- src/com/monday/OurDateWriterReader.java
+++ src/com/monday/OurDateWriterReader.java
@@ -2,13 +2,14 @@ package com.monday;
 
 import java.io.*;
 import java.nio.file.*;
-import java.time.LocalDateTime;
+import java.time.LocalDateTime; 
+
 
 public class OurDateWriterReader {
 
 	public static void main(String[] args) {
 		
-		Path aNewPath = Paths.get("\\Users\\erine\\desktop\\everyTimeWeRanApp.txt");
+		Path aNewPath = Paths.get("\\Users\\erine\\SSAResart\\WeekThree\\src\\com\\monday\\mycountries.txt");
 		File ourNewFile = aNewPath.toFile();
 		
 		try (PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(ourNewFile, true)))) {
